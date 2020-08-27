f767ce3f7490173a3e8f4f6daa970a60c4efe51d
Kshitij KJO1. Joshi <kshitij.joshi@softtekge.com>
2020-08-27 20:08:49 +0530

    stash test

--- GitTest/src/com/gittest/app/PrintNo.java
+++ GitTest/src/com/gittest/app/PrintNo.java
@@ -1,7 +1,7 @@
 package com.gittest.app;
 
 public class PrintNo {
-
+	// this is a comment in v2
 	public static void main(String[] args) {
 		for (int i = 0; i < 12; i++) {
 			System.out.println(i+1);
