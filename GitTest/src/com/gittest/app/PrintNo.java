a681fa22d48c53535c5431189b75cb7001215475
Kshitij Joshi <39580056+JoshiKshitij@users.noreply.github.com>
2020-09-01 16:11:04 +0530

    Update PrintNo.java
    divide commit

--- GitTest/src/com/gittest/app/PrintNo.java
+++ GitTest/src/com/gittest/app/PrintNo.java
@@ -3,7 +3,9 @@ package com.gittest.app;
 public class PrintNo {
 
 	public static void main(String[] args) {
+		
 		for (int i = 0; i < 12; i++) {
+			if(i/2 ==0)
 			System.out.println(i+1);
 		}
 	}
