18f0e6595fa5b3c94ed7e0b8d1cac01c16707e37
kshitij.joshi <kshitij.joshi@192.168.56.1>
2020-09-01 16:16:15 +0530

    moduls

--- GitTest/src/com/gittest/app/PrintNo.java
+++ GitTest/src/com/gittest/app/PrintNo.java
@@ -4,6 +4,7 @@ public class PrintNo {
 	// this is a comment in v2
 	public static void main(String[] args) {
 		for (int i = 0; i < 12; i++) {
+			if(i % 2 == 0)
 			System.out.println(i+1);
 		}
 	}
